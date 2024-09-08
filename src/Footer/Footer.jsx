7deee7edde2cc3162a8633b2623ccd79cc2681d2
ugonnachidinmac <ugonnachidinma92@gmail.com>
2024-09-09 00:53:01 +0100

    update form functionality

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,51 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Footer = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent form submission
+    alert("SUBSCRIBE successfully");
+    setEmail(""); // Clear the input after submission
+  };
+
   return (
- <>
-  <div className="footer">
-    <div className="insideFooter">
-      <div className="footerInfo">
+    <>
+      <div className="footer">
+        <div className="insideFooter">
+          <div className="footerInfo">
+            <div className='leftSide'>
+              <h4>Funiro.</h4>
+              <p>400 University Drive Suite 200 Coral <br /> Gables,<br />
+                FL 33134 USA</p>
+            </div>
 
-        <div className='leftSide'>
-          <h4>Funiro.</h4>
-          <p>400 University Drive Suite 200 Coral <br /> Gables,<br />
-          FL 33134 USA</p>
-        </div>
+            <div className='listFormFlex'>
+              <div className='linksList'>
+                <ul>
+                  <li className='heads'> Links </li>
+                  <li>Home</li>
+                  <li>Shop</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+                <ul>
+                  <li className='heads'> Help </li>
+                  <li>Payment Options</li>
+                  <li>Returns</li>
+                  <li>Privacy Policies</li>
+                </ul>
+              </div>
 
-        <div className='listFormFlex'>
-        <div className='linksList'>
-          <ul>
-            <li className='heads'> Links </li>
-            <li>Home</li>
-            <li>Shop</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-          <ul>
-            <li className='heads'> Help </li>
-            <li>Payment Options</li>
-            <li>Returns</li>
-            <li>Privacy Policies</li>
-          </ul>
+              <form onSubmit={handleSubmit}>
+                <fieldset>
+                  <p className='heads'>Newsletter</p>
+                  <fieldset className='fieldsetFlex'>
+                    <input
+                      type="email"
+                      placeholder='Enter Your Email Address'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                    <button type="submit">SUBSCRIBE</button>
+                  </fieldset>
+                </fieldset>
+              </form>
+
+            </div>
+          </div>
         </div>
-        <form action="">
-          <fieldset>
-            <p className='heads'>Newsletter</p>
-          <fieldset className='fieldsetFlex'>
-            <input type="email" placeholder='Enter Your Email Address' />
-            <button>SUBSCRIBE</button>
-          </fieldset>
-          </fieldset>
-      </form>
-        </div>
+        <div className='lastContent'>2023 Funiro. All rights reserved</div>
       </div>
-    </div>
-    <div className='lastContent'>2023 furino. All rights reverved</div>
-  </div>
- 
- </>
-  )
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Section3/Section3.jsx
+++ src/Section3/Section3.jsx
@@ -1,91 +1,63 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import SwiperCore, { Autoplay, Navigation, Pagination } from 'swiper';
-import img41 from '../assets/img41.png'; // replace with your image paths
-import img42 from '../assets/img42.png';
-import img43 from '../assets/img43.png';
-import arrowrightpoint from '../assets/arrowrightpoint.png';
-import dot1 from '../assets/dot1.png';
-import dot2 from '../assets/dot2.png';
-
-// Initialize Swiper modules
-SwiperCore.use([Autoplay, Navigation, Pagination]);
+import React from "react";
+import img41 from "../assets/img41.png";
+import hori from "../assets/hori.png";
+import arrowRight from "../assets/arrowRight.png";
+import img42 from "../assets/img42.png";
+import arrowrightpoint from "../assets/arrowrightpoint.png";
+import dot1 from "../assets/dot1.png";
+import dot2 from "../assets/dot2.png";
+import img43 from "../assets/img43.png";
 
 const Section3 = () => {
   return (
     <>
       <div className="section3Container">
-        <Swiper
-          spaceBetween={30}
-          centeredSlides={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-            el: '.dotsContainer',
-          }}
-          navigation={{
-            nextEl: '.arrowrightpoint',
-          }}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <div className="sec3Row1">
-              <p>
-                50+ Beautiful rooms <br />
-                inspiration
-              </p>
-              <p>
-                Our designer already made a lot of beautiful prototype rooms
-                that inspire you
-              </p>
-              <button>Explore More</button>
-            </div>
-            <div className="sec3Row2">
-              <img src={img41} alt="img41" />
-              <div className="bgWhiteContent">
-                <div className="containerRoom1">
-                  <div className="room1">
-                    <p>01</p>
-                    <img src={img41} alt="hori" />
-                    <p>Bed Room</p>
-                  </div>
-                  <p>Inner Peace</p>
-                </div>
-                <div className="arrowRight">
-                  <img src={arrowrightpoint} alt="Arrow" />
-                </div>
+        <div className="sec3Row1">
+          <p>
+            50+ Beautiful rooms <br />
+            inspiration
+          </p>
+          <p>
+            Our designer already made a lot of beautiful prototipe of rooms that
+            inspire you
+          </p>
+          <button>Explore More</button>
+        </div>
+
+        <div className="sec3Row2">
+          <img src={img41} alt="img41" />
+          <div className="bgWhiteContent">
+            <div className="containerRoom1">
+              <div className="room1">
+                <p>01</p>
+                <img src={hori} alt="" />
+                <p>Bed Room</p>
               </div>
+              <p>Inner Peace</p>
             </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className="sec3MiddleTop">
-              <div className="imgContainer">
-                <img className="img42" src={img42} alt="Room" />
-                <div className="arrowrightpoint">
-                  <img src={arrowrightpoint} alt="Next" />
-                </div>
-              </div>
+            <div className="arrowRight">
+              <img src={arrowRight} alt="" />
             </div>
-          </SwiperSlide>
+          </div>
+        </div>
 
-          <SwiperSlide>
-            <div>
-              <img src={img43} alt="Room" />
+        <div className="sec3MiddleTop">
+          <div className="imgContainer">
+            <img className="img42" src={img42} alt="" />
+            <div className="arrowrightpoint">
+              <img src={arrowrightpoint} alt="" />
             </div>
-          </SwiperSlide>
-        </Swiper>
+          </div>
+          <div className="dotsContainer">
+            <img src={dot1} alt="" />
+            <img src={dot2} alt="" />
+            <img src={dot2} alt="" />
+            <img src={dot2} alt="" />
+          </div>
+        </div>
 
-        <div className="dotsContainer">
-          <img src={dot1} alt="dot1" />
-          <img src={dot2} alt="dot2" />
-          <img src={dot2} alt="dot2" />
-          <img src={dot2} alt="dot2" />
+        <div>
+          <img src={img43} alt="" />
         </div>
       </div>
     </>
