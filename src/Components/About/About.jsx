2100cb4e944b79baf521eab33f43a26d3d0f5fb2
ugonnachidinmac <ugonnachidinma92@gmail.com>
2024-10-02 01:01:42 +0100

    spicing aboutn page

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <>
-    <section className='flex flex-col item-center justify-center m-auto'>
-    <h1 className='text-center text-blue-700 text-[28px] border border-green-2'>CareerEx</h1>
-    <div className='w-[750px] border border-l-blue-900 m-auto ml-[350px] pl-[30px]'>
-      <h2 className='font-bold text-[24px] mb-[15px]'>About us?</h2>
-      <div className='text-justify'>
-      At CareerEx, we transform career ambitions into achievements. We make you a coveted professional no recruiter can overlook. From your application till you ace that dream interview, we’re with you every step of the way!
-      </div>
-    </div>
-    <div className='w-[750px] border border-l-blue-900 m-auto ml-[350px] pl-[30px] mt-[50px]'>
-      <h2 className='font-bold text-[24px] mb-[15px]'>Why subscribe to our newsletter?</h2>
-      <div className='text-justify'>
-      We publish career tips to help you stay ahead of the curve in your professional journey! Whether you’re just kickstarting your career as a student, a recent graduate; you’re a young professional, or you’re seeking to transition to a new path, our publications are targeted towards making you a coveted professional no recruiter can overlook!
-      </div>
-    </div>
-    </section>
-    </>
-  )
-}
+    <section className="flex flex-col items-center justify-center m-auto w-full max-w-screen-lg px-4">
+      {/* Header Section */}
+      <h1 className="text-center text-blue-700 text-2xl lg:text-3xl font-semibold border-b-2 border-red-500 w-full my-6">
+        CareerEx
+      </h1>
 
-export default About
+      {/* About Us Section */}
+      <div className="border-l-4 border-blue-900 w-full lg:w-3/4 p-6 mb-8">
+        <h2 className="font-bold text-xl lg:text-2xl mb-4">About us</h2>
+        <p className="text-justify text-gray-700">
+          At CareerEx, we transform career ambitions into achievements. We make you a coveted professional no recruiter can overlook. From your application till you ace that dream interview, we’re with you every step of the way!
+        </p>
+      </div>
+
+      {/* Newsletter Section */}
+      <div className="border-l-4 border-blue-900 w-full lg:w-3/4 p-6 mt-8">
+        <h2 className="font-bold text-xl lg:text-2xl mb-4">Why subscribe to our newsletter?</h2>
+        <p className="text-justify text-gray-700">
+          We publish career tips to help you stay ahead of the curve in your professional journey! Whether you’re just kickstarting your career as a student, a recent graduate, a young professional, or you’re seeking to transition to a new path, our publications are targeted towards making you a coveted professional no recruiter can overlook!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
